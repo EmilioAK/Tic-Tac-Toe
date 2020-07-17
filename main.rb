44151fb31dc57a9f3809ab9ffab6409071c48cee
EmilioAK <emilio.alvarez96@gmail.com>
2020-07-17 02:50:21 +0200

    Add main file that runs the program

--- main.rb
+++ main.rb
@@ -1,39 +1,19 @@
-require_relative 'game_board.rb'
-require_relative 'game_rules.rb'
+require_relative 'round.rb'
 
-class Round
-    include GameRules
+def main
+    player1_score = 0
+    player2_score = 0
     
-    def initialize
-        @whos_turn = 1 # Is either 0 or 1
-        @game_board = GameBoard.new
-    end
+    loop do
+        round = Round.new
+        winner = round.play_round
 
-    def increment_turn
-        @whos_turn = (@whos_turn + 1) % 2
-    end
-
-    def play_turn
-        @game_board.print_board
-        print "Enter placement: "        
-        user_input = gets.chomp.split(",").map(&:to_i) # Makes the input a list of ints
-        
-        begin
-            @game_board.insert_piece(@whos_turn, *user_input)
-        rescue => placement_error
-            puts placement_error
-            play_turn # This solution prevents increment_turn from running
-        end
-    end
-
-    def play_round
-        until round_won?(@game_board.board) do
-            increment_turn
-            play_turn 
-        end
-        puts "Player #{@whos_turn + 1} won!"
+        winner = 1 ? player1_score += 1 : player2_score += 1
+        puts "Player 1's score is: #{player1_score} \n"
+        puts "Player 2's score is: #{player2_score}"
     end
 end
 
-round = Round.new
-round.play_round
+if $0 == __FILE__
+    main
+end
---
+++ round.rb
@@ -0,0 +1,37 @@
+require_relative 'game_board.rb'
+require_relative 'game_rules.rb'
+
+class Round
+    include GameRules
+    
+    def initialize
+        @whos_turn = 1 # Is either 0 or 1
+        @game_board = GameBoard.new
+    end
+
+    def increment_turn
+        @whos_turn = (@whos_turn + 1) % 2
+    end
+
+    def play_turn
+        @game_board.print_board
+        print "Enter placement: "        
+        user_input = gets.chomp.split(",").map(&:to_i) # Makes the input a list of ints
+        
+        begin
+            @game_board.insert_piece(@whos_turn, *user_input)
+        rescue => placement_error
+            puts placement_error
+            play_turn # This solution prevents increment_turn from running
+        end
+    end
+
+    def play_round
+        until round_won?(@game_board.board) do
+            increment_turn
+            play_turn 
+        end
+        puts "Player #{@whos_turn + 1} won!"
+        return @whos_turn + 1
+    end
+end
