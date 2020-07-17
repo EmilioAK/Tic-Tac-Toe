6df8aaf402c53efdeb6c1ee79cc1faaa4a41bb6e
EmilioAK <emilio.alvarez96@gmail.com>
2020-07-17 03:00:51 +0200

    Fix \n in main

--- main.rb
+++ main.rb
@@ -9,8 +9,8 @@ def main
         winner = round.play_round
 
         winner = 1 ? player1_score += 1 : player2_score += 1
-        puts "Player 1's score is: #{player1_score} \n"
-        puts "Player 2's score is: #{player2_score}"
+        puts "Player 1's score is: #{player1_score} "
+        puts "Player 2's score is: #{player2_score} \n\n" #I have no idea why I need two \n here, but it doesn't work otherwise
     end
 end
 
